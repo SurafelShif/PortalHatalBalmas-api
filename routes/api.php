4ecc942cb1995509a0b5b5b59011a3220e2ba468
Surafel <surafelshif@gmail.com>
2025-02-16 11:40:48 +0200

    remove: middleware for the routes

--- routes/api.php
+++ routes/api.php
@@ -17,50 +17,90 @@ Route::controller(AuthController::class)->group(function () {
 Route::controller(GlobalController::class)->group(function () {
     Route::get('/search', 'search');
 });
-Route::controller(UserController::class)->middleware(['auth:api', 'role:admin'])->prefix('users')->group(function () {
+// Route::controller(UserController::class)->middleware(['auth:api', 'role:admin'])->prefix('users')->group(function () {
+//     Route::post('/', 'addAdmin');
+//     Route::delete('/{personal_id}', 'deleteAdmin');
+//     Route::get('/', 'index');
+// });
+Route::controller(UserController::class)->prefix('users')->group(function () {
     Route::post('/', 'addAdmin');
     Route::delete('/{personal_id}', 'deleteAdmin');
     Route::get('/', 'index');
 });
-Route::controller(PostController::class)->middleware(['auth:api', 'role:admin'])->prefix('posts')->group(function () {
+// Route::controller(PostController::class)->middleware(['auth:api', 'role:admin'])->prefix('posts')->group(function () {
+//     Route::post('/', 'createPost');
+//     Route::post('/{uuid}', 'updatePost');
+//     Route::delete('/{uuid}', 'deletePost');
+//     Route::middleware("throttle:20,1")->withoutMiddleware(['auth:api', 'role:admin'])->group(function () {
+//         Route::get('/', 'getPosts');
+//         Route::get('/{uuid}', 'getPostByUUid');
+//     });
+// });
+Route::controller(PostController::class)->prefix('posts')->group(function () {
     Route::post('/', 'createPost');
     Route::post('/{uuid}', 'updatePost');
     Route::delete('/{uuid}', 'deletePost');
-    Route::middleware("throttle:20,1")->withoutMiddleware(['auth:api', 'role:admin'])->group(function () {
-        Route::get('/', 'getPosts');
-        Route::get('/{uuid}', 'getPostByUUid');
-    });
+    Route::get('/', 'getPosts');
+    Route::get('/{uuid}', 'getPostByUUid');
 });
 
-Route::controller(AnnouncementController::class)->middleware(['auth:api', 'role:admin'])->prefix('announcements')->group(function () {
+// Route::controller(AnnouncementController::class)->middleware(['auth:api', 'role:admin'])->prefix('announcements')->group(function () {
+//     Route::post('/update', 'updateAnnouncement');
+//     Route::middleware("throttle:20,1")->patch('/{uuid}', 'updateAnnouncementVisibility');
+//     Route::post('/', 'createAnnouncement');
+//     Route::delete('/{uuid}', 'deleteAnnouncement');
+//     Route::get('/admin', 'getAdminAnnouncements');
+//     Route::withoutMiddleware(['auth:api', 'role:admin'])->group(function () {
+//         Route::get('/', 'getAnnouncements');
+//         Route::get('/{uuid}', 'getAnnouncementByUUid');
+//     });
+// });
+Route::controller(AnnouncementController::class)->prefix('announcements')->group(function () {
     Route::post('/update', 'updateAnnouncement');
     Route::middleware("throttle:20,1")->patch('/{uuid}', 'updateAnnouncementVisibility');
     Route::post('/', 'createAnnouncement');
     Route::delete('/{uuid}', 'deleteAnnouncement');
     Route::get('/admin', 'getAdminAnnouncements');
-    Route::withoutMiddleware(['auth:api', 'role:admin'])->group(function () {
-        Route::get('/', 'getAnnouncements');
-        Route::get('/{uuid}', 'getAnnouncementByUUid');
-    });
+    Route::get('/', 'getAnnouncements');
+    Route::get('/{uuid}', 'getAnnouncementByUUid');
 });
-Route::controller(InformationController::class)->middleware(['auth:api', 'role:admin'])->prefix('info')->group(function () {
+// Route::controller(InformationController::class)->middleware(['auth:api', 'role:admin'])->prefix('info')->group(function () {
+//     Route::post('/', 'createInformation');
+//     Route::delete('/{uuid}', 'deleteInformation');
+//     Route::post('/{uuid}', 'updateInformation');
+//     Route::withoutMiddleware(['auth:api', 'role:admin'])->group(function () {
+//         Route::get('/', 'getInformations');
+//         Route::get('/{uuid}', 'getInformationByUUid');
+//     });
+// });
+Route::controller(InformationController::class)->prefix('info')->group(function () {
     Route::post('/', 'createInformation');
     Route::delete('/{uuid}', 'deleteInformation');
     Route::post('/{uuid}', 'updateInformation');
-    Route::withoutMiddleware(['auth:api', 'role:admin'])->group(function () {
-        Route::get('/', 'getInformations');
-        Route::get('/{uuid}', 'getInformationByUUid');
-    });
+    Route::get('/', 'getInformations');
+    Route::get('/{uuid}', 'getInformationByUUid');
 });
-Route::controller(CategoryController::class)->middleware(['auth:api', 'role:admin'])->prefix('categories')->group(function () {
+// Route::controller(CategoryController::class)->middleware(['auth:api', 'role:admin'])->prefix('categories')->group(function () {
+//     Route::post('/', 'createCategory');
+//     Route::delete('/{categoryId}', 'deleteCategory');
+//     Route::put('/{categoryId}', 'updateCategory');
+//     Route::withoutMiddleware(['auth:api', 'role:admin'])->get('/', 'getCategories');
+// });
+Route::controller(CategoryController::class)->prefix('categories')->group(function () {
     Route::post('/', 'createCategory');
     Route::delete('/{categoryId}', 'deleteCategory');
     Route::put('/{categoryId}', 'updateCategory');
-    Route::withoutMiddleware(['auth:api', 'role:admin'])->get('/', 'getCategories');
+    Route::get('/', 'getCategories');
 });
-Route::controller(SiteController::class)->middleware(['auth:api', 'role:admin'])->prefix('sites')->group(function () {
+// Route::controller(SiteController::class)->middleware(['auth:api', 'role:admin'])->prefix('sites')->group(function () {
+//     Route::post('/', 'createSite');
+//     Route::post('/{uuid}', 'updateSite');
+//     Route::delete('/{uuid}', 'deleteSite');
+//     Route::withoutMiddleware(['auth:api', 'role:admin'])->get('/', 'getSites');
+// });
+Route::controller(SiteController::class)->prefix('sites')->group(function () {
     Route::post('/', 'createSite');
     Route::post('/{uuid}', 'updateSite');
     Route::delete('/{uuid}', 'deleteSite');
-    Route::withoutMiddleware(['auth:api', 'role:admin'])->get('/', 'getSites');
+    Route::get('/', 'getSites');
 });
