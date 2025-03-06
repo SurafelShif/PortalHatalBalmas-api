ddb85b8f5626218a46e7fcf822f61ce301bc97b5
Surafel <surafelshif@gmail.com>
2025-03-06 14:03:16 +0200

    adjust: AddAdminRequest

--- app/Http/Requests/AddAdminRequest.php
+++ app/Http/Requests/AddAdminRequest.php
@@ -32,8 +32,8 @@ class AddAdminRequest extends FormRequest
     public function messages()
     {
         return [
-            'full_name.required' => 'שם המשתמש חסר',
-            'full_name.min' => 'שם המשתמש צריך להיות לפחות שני תווים',
+            'fullName.required' => 'שם המשתמש חסר',
+            'fullName.min' => 'שם המשתמש צריך להיות לפחות שני תווים',
             'personal_id.required' => 'תעודת הזהות חסר',
             'personal_id.integer' => 'תעודת הזהות אינה בפורמט הנכון',
             'personal_id.regex' => 'תעודת הזהות צריכה להיות 9 תווים',
