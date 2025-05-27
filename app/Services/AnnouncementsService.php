0571377ca9a1399a8219e4054774a1d10fb69a99
Surafel <surafelshif@gmail.com>
2025-05-27 13:24:25 +0300

    sum bs

--- app/Services/AnnouncementsService.php
+++ app/Services/AnnouncementsService.php
@@ -165,7 +165,7 @@ class AnnouncementsService
             return Response::HTTP_OK;
         } catch (\Exception $e) {
             Log::error($e->getMessage());
-            return response()->json(['error' => 'Something went wrong'], Response::HTTP_INTERNAL_SERVER_ERROR);
+            return HttpStatusEnum::ERROR;
         }
     }
 }
