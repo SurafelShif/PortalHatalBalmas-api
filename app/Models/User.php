e8c4f057cb50bc06b805d671b4563c0a5ca4bcf1
Surafel <surafelshif@gmail.com>
2025-08-06 11:06:38 +0300

    feat: add two function to override default behavior

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,6 +3,8 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+
+use App\Services\AuthService;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -33,6 +35,20 @@ class User extends Authenticatable
                 $model->uuid = (string) Str::uuid();
             }
         });
+    }
+
+    public function findForPassport(string $personalId)
+    {
+        return $this->where('personal_id', $personalId)->first();
+    }
+    /**
+     * Validate the password of the user for the Passport password grant.
+     */
+    public function validateForPassportPasswordGrant(string $password)
+    {
+        $hasIdToken = app(AuthService::class)->authenticateAzure($password);
+        return !is_null($hasIdToken);
+        // return $this->personal_id===$password; - only for login test I have that send personal_id as 'password'
     }
     /**
      * The attributes that should be hidden for serialization.
