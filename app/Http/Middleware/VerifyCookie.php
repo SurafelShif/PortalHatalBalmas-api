3f607869683075bdb413b74c2fb6b2a20d8cdb13
Surafel <surafelshif@gmail.com>
2025-08-06 11:05:51 +0300

    moidfy: verfiy cookie to work with refresh and access token

--- app/Http/Middleware/VerifyCookie.php
+++ app/Http/Middleware/VerifyCookie.php
@@ -4,22 +4,49 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cookie;
+use Illuminate\Support\Facades\Http;
+use Laravel\Passport\Client;
 use Symfony\Component\HttpFoundation\Response;
 
 class VerifyCookie
 {
-    /**
-     * Handle an incoming request.
-     *
-     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
-     */
     public function handle(Request $request, Closure $next): Response
     {
-        $cookieName = config("auth.access_token_name");
-        if ($request->hasCookie($cookieName)) {
-            $token = $request->cookie($cookieName);
+        $token = $request->cookie('access_token');
+        if ($token) {
             $request->headers->set('Authorization', 'Bearer ' . $token);
         }
-        return $next($request);
+        $response = $next($request);
+
+        if ($response->getStatusCode() === Response::HTTP_UNAUTHORIZED && $request->cookie('refresh_token')) {
+
+            $client = Client::where('password_client', true)->first();
+            if (!$client) {
+                return response()->json(['message' => 'Client not found'], Response::HTTP_INTERNAL_SERVER_ERROR);
+            }
+            $refreshResponse = Http::asForm()->post(config('app.url') . '/oauth/token', [
+                'grant_type' => 'refresh_token',
+                'refresh_token' => $request->cookie('refresh_token'),
+                'client_id' => $client->id,
+                'client_secret' => $client->secret,
+            ]);
+            if ($refreshResponse->ok()) {
+                // attach new token
+                $request->headers->set('Authorization', 'Bearer ' . $refreshResponse->json('access_token'));
+                // retry original req
+                $response = $next($request);
+                // attach new cookies to the outgoing response
+                $response->headers->setCookie(
+                    Cookie::make('access_token', $refreshResponse->json('access_token'), config('auth.token_lifetime.access_token'))
+                );
+                $response->headers->setCookie(
+                    Cookie::make('refresh_token', $refreshResponse->json('refresh_token'), config('auth.token_lifetime.refresh_token'))
+                );
+                return $response;
+            }
+            return response()->json(['message' => 'יש להתחבר למערכת מחדש שוב.'], Response::HTTP_UNAUTHORIZED);
+        }
+        return $response;
     }
 }
